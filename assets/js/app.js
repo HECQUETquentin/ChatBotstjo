39b78c539fab3eb1b11d3335368983ebd1517180
Quentin Hecquet <quentin.hecquet@outlook.fr>
2023-05-10 12:27:07 +0200

    update code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,25 @@
-const dialog=[{question:"toto",answer:"tata"}, {question:"Salut", answer:"Salut"}, {question:"ça va ?", answer:"bien et toi ?"}]
+const dialogs=[
+    {question:"toto",answer:"tata"}, 
+    {question:"Salut", answer:"Salut"}, 
+    {question:"ça va ?", answer:"bien et toi ?"}
+]
 
-document.getElementById("form-api").addEventListener("submit", function(event){
+const datalist = document.querySelector("#datalist");
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
+const inputdialog = document.querySelector("#inputdialog");
+
+inputdialog.addEventListener("change", function(event){
     event.preventDefault();
-    document.querySelector(".question").innerHTML=document.querySelector(".form-question").value
-    document.querySelector(".answer").innerHTML="Coucou"
-    dialog.forEach(element =>{
+    question.innerHTML=document.querySelector(".form-question").value
+    answer.innerHTML="Coucou"
+    dialogs.forEach(element =>{
         if(element.question === document.querySelector(".form-question").value){
-            document.querySelector(".answer").innerHTML=element.answer
+            answer.innerHTML=element.answer
         }
     })
 })
+
+dialogs.forEach((dialog) =>{
+    datalist.innerHTML += `<option value="${dialog.question}">`;
+});
