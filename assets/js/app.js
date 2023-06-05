8c31e461d83ba6a786b18753dbd9800547ddcc0f
Quentin Hecquet <quentin.hecquet@outlook.fr>
2023-06-05 09:42:17 +0200

    frontupdate

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,50 @@
+const datalist = document.querySelector("#datalist");
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
+const inputdialog = document.querySelector("#inputdialog");
+
+/*let contentAnswer = document.querySelector("#dialoganswer");
+const word = contentAnswer.textContent;
+const length = word.length;
+let count = 0;
+contentAnswer.textContent = "";
+
+const write = setInterval(() => {
+    contentAnswer.textContent += word[count]
+    count += 1;
+    if(count > length -1){
+        clearInterval(write)
+    }
+    console.log(count);
+}, 100);
+*/
+console.log("coucou from apijs")
+fetch("https://chatbotstjoback.onrender.com/api/v1/dialogs")
+.then(response=>response.json())
+.then(data=>{
+    data.message.forEach(dialog => {
+        datalist.innerHTML += `<option value="${dialog.question}">`
+    })
+
+    inputdialog.addEventListener("change", e=>{
+        question.innerHTML=document.querySelector(".form-question").value
+        data.message.forEach(dialog =>{
+            if(e.target.value === dialog.question){
+                console.log(dialog.answer)
+                answer.innerHTML = dialog.answer
+            }
+        })
+    })
+})
+.catch(error=>console.log(error))
+
 const dialogs=[
     {question:"toto",answer:"tata"}, 
     {question:"Salut", answer:"Salut"}, 
     {question:"ça va ?", answer:"bien et toi ?"}
 ]
 
-const datalist = document.querySelector("#datalist");
-const question = document.querySelector(".question");
-const answer = document.querySelector(".answer");
-const inputdialog = document.querySelector("#inputdialog");
-
-inputdialog.addEventListener("change", function(event){
+/*inputdialog.addEventListener("change", function(event){
     event.preventDefault();
     question.innerHTML=document.querySelector(".form-question").value
     answer.innerHTML="Coucou"
@@ -22,4 +57,4 @@ inputdialog.addEventListener("change", function(event){
 
 dialogs.forEach((dialog) =>{
     datalist.innerHTML += `<option value="${dialog.question}">`;
-});
+});*/
